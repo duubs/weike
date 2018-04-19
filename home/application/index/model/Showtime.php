7e9752edd1f9be72d881bed20a2a7fdaf94d256c
hanguanghao <493530992@qq.com>
2018-04-19 16:11:28 +0800

    前台试听

---
+++ home/application/index/model/Showtime.php
@@ -0,0 +1,22 @@
+<?php 
+namespace app\index\model;
+use think\Model;
+use think\Db;
+use think\paginate;
+
+class Showtime extends Model
+{
+	// // protected $table='good';
+	// public function showList()
+	// {
+	// 	$result=Db::table('micro_course')
+	// 	->alias('c')
+	// 	->
+	// 	->select();
+	// 	return $result;
+	// }
+
+	// public function select_one(){
+	// 	$res=
+	// }
+}
