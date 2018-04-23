c32dce80a135536942e48cf8c5eee12420773742
hanguanghao <493530992@qq.com>
2018-04-23 09:21:20 +0800

    试听

---
+++ home/application/index/controller/Audition.php
@@ -0,0 +1,59 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use think\Db;
+use think\View;
+use think\Request;
+// use app\index\model\showtime;
+class Audition extends Controller
+{
+	/*
+	*  首页
+	*/
+    public function audition()
+    {
+        //查找所有高中初中 试听页面所需数据
+        $course =Db::table('micro_course')
+                ->field('course_name,teacher_name,subject_name,stage_name,course_img,course_id')
+                ->select();     
+
+        return view('audition',['course' => $course]);
+
+    }
+  
+
+     public function ajaxStage()
+    {
+        //根据传回的id号 1 2 对应高中、初中试听课内容
+        $stage_id = Request::instance()->get('data');     
+        $stage = Db::table('micro_course')
+                ->field('course_name,teacher_name,subject_name,stage_name,course_img,course_id')
+                ->where('stage_id',$stage_id)
+                ->select(); 
+        return $stage;
+    }
+
+         public function ajaxLittle()
+    {
+        //调初中试听课内容
+        $stage_id = Request::instance()->get('data');    
+        $stage = Db::table('micro_course')
+                ->field('course_name,stage_name,subject_name,course_img,course_id')
+                ->where('stage_id',$stage_id)
+                ->select();      
+        return $stage;
+    }
+
+         public function select()
+    {
+        //点击图片 到图片（应为video）页面
+          $course_id = Request::instance()->get('id');
+          $showtime = Db::table('micro_course')
+                ->where('course_id',$course_id)
+                ->select();  
+        return view('showtime',['showtime' => $showtime]);     
+
+    }
+
+}
---
+++ home/application/index/view/audition/audition.php
@@ -0,0 +1,240 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>微课网  - 试听专区 | 微课网在线教育 国内首家中学生ESNS视频学习社区。加入微课网，学的不只是好课，实现高效学习，快乐分享。</title>
+    <meta http-equiv="Access-Control-Allow-Origin" content="*" />
+	<meta name="alexaVerifyID" content="COpV9RoXtcanUi3TXPNlodOTxuw" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge"> 
+<link href="http://static.vko.cn/common/css/base.css" rel="stylesheet" type="text/css" media="screen"/>
+
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/web/new-header.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/v8s/common/css/headerTest.css" />
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/web/top_common.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/web/common-inherit.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/pre-common.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/ie.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/print.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/screen.css">
+<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/tiku/css/pageBar.css">
+<script type="text/javascript">
+	var apiPath = "http://apitest.vko.cn"; 
+	var goodsPath = 'http://www.vko.cn';
+	var payPath = 'http://pay.vko.cn';
+	var studyPath="http://studycenter.vko.cn";
+	var tikuPath="http://tiku.vko.cn";
+	//var htmPath="http://www.vko.cn/learning";
+	var htmPath="http://static.vko.cn/v8/v8s/learning";
+</script>
+    <link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/v8s/common/css/common.css" />
+    <link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/v8s/learning/css/course.css" />
+    <link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/v8s/learning/css/try.css" />
+    <script type="text/javascript" src="http://static.vko.cn/v8/common/jqueryplugin/easyui/jquery-1.7.2.min.js"></script>
+    <meta name="Keywords" content="课程活动,教育活动,打折课程,免费活动,免费试听,微课网,在线教育,网校,学习网,高考,中考,VKO,ESNS">
+    <meta name="Description" content="微课网活动频道是一个提供各式各样教育活动的平台。包含中小学课程打折，免费活动，免费试听等等，微课网 国内首家中学生ESNS学习社交网络，着眼于中考、高考命题的深刻解析，提供丰富的初高中各学科的在线教育名师微课程视频，在这里你可以和同学组成圈子共同学习、互动答疑、测试并分享学习动态！微课网热线：4008-900-800。">
+
+
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/web/common.css">
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/web/top_common.css">
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/web/common-inherit.css">
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/pre-common.css">
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/ie.css">
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/print.css">
+	<link rel="stylesheet" type="text/css" href="http://static.vko.cn/v8/common/css/screen.css">
+
+	<script type="text/javascript" src="http://static.vko.cn/common/js/stacktrace.js"></script>
+	<script type="text/javascript" src="http://static.vko.cn/common/js/vkofileloader.js"></script>
+	<script type="text/javascript" src="http://static.vko.cn/common/js/seajs/seajs/2.2.0/sea.js"></script>
+	<script type="text/javascript" src="http://static.vko.cn/common/js/config.js"></script>
+</head>
+<body>
+
+<script type="text/javascript">
+vkoFileLoader.load('/vko/css/special/try/tryv8.css');
+</script>
+<!--筛选 start-->
+<div class="courseItemBox">
+    <div class="contList">
+        <div class="list borderList clearfix">
+            <h2>首页<b>&gt;</b></h2>
+            <ul class="tagList2">
+                <li><a>试听</a></li>
+            </ul>
+        </div>
+        <div class="list clearfix">
+            <h2>学段 <b>&gt;</b></h2>
+            <ul class="listen_Check_Grade">
+                <li><a  data="51" value="1" id="high">高中</a></p></li>
+                <li><a  data="52" value="2" id="first">初中</a></li>
+                <li><a  data="53" value="3" id="little">小学</a></li>
+            </ul>
+        </div>
+            <div class="tryMain clearfix" id="senior">
+                <?php foreach ($course as $key => $value) { ?>
+                 <div class="tryList">
+                    <div class="tryPic">
+                        <a href="{:url('Audition/select')}?id=<?=$value['course_id']?>" uv="7ac4c9e3e9" vid="5fd714ac3217cd34f3774c1ac88ddf37_5">
+                            <img src="http:_PUBLIC_<?=$value['course_img']?>" title="考场好作文(一)写作的本质" />
+                        </a>
+                        <span class="sign grades"><?=$value['stage_name']?></span>
+                        <span class="sign object"><?=$value['subject_name']?></span>
+                    </div>
+                    <div class="tryIntro">
+                        <h6><?=$value['course_name'] ?></h6>
+                        <p>主讲：<span><?=$value['teacher_name'] ?></span></p>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+
+
+    </div>
+</div>
+<!--试听 start-->
+<!-- 高中试听 -->
+
+<srcipt src="_PUBLIC_/js/jquery.js"></srcipt>
+<script>
+    $(function(){
+        //高中
+        $('#high').click(function(){
+           
+            // var high = $('#high').val();
+            $.get("/index/audition/ajaxStage",{data:1},function(data){
+             html = ""
+                // alert(data[1]['course_name']);
+                $.each( data, function(i,v){
+                        // alert(11)
+                                    html += 
+                                    '    <div class="tryList">\
+                    <div class="tryPic">\
+                        <a href="{:url('Audition/select')}?id=<?=$value['course_id']?>" uv="7ac4c9e3e9" vid="5fd714ac3217cd34f3774c1ac88ddf37_5">\
+                            <img src="http:_PUBLIC_'+v.course_img+'" title="考场好作文(一)写作的本质" />\
+                        </a>\
+                        <span class="sign grades">'+v.stage_name+'</span>\
+                        <span class="sign object">'+v.subject_name+'</span>\
+                    </div>\
+                    <div class="tryIntro">\
+                        <h6>'+v.course_name+'</h6>\
+                        <p>主讲：<span>'+v.teacher_name+'</span></p>\
+                    </div>\
+                </div>\
+                ';
+                });
+
+                // alert(html)
+                $('#senior').html(html);
+                
+            })
+        })
+
+        //初中
+        $('#first').click(function(){
+            
+            // var high = $('#high').val();
+            $.get("/index/audition/ajaxStage",{data:2},function(data){
+         html = ""
+                // alert(data[1]['course_name']);
+                $.each( data, function(i,v){
+                        // alert(11)
+                                    html += 
+                                    '    <div class="tryList">\
+                    <div class="tryPic">\
+                        <a href="{:url('Audition/select')}?id=<?=$value['course_id']?>" uv="7ac4c9e3e9" vid="5fd714ac3217cd34f3774c1ac88ddf37_5">\
+                            <img src="http:_PUBLIC_'+v.course_img+'" title="考场好作文(一)写作的本质" />\
+                        </a>\
+                        <span class="sign grades">'+v.stage_name+'</span>\
+                        <span class="sign object">'+v.subject_name+'</span>\
+                    </div>\
+                    <div class="tryIntro">\
+                        <h6>'+v.course_name+'</h6>\
+                        <p>主讲：<span>'+v.teacher_name+'</span></p>\
+                    </div>\
+                </div>\
+                ';
+                });
+                // alert(html)
+                $('#senior').html(html);
+
+                
+            })
+        })
+    
+            //小学
+     $('#little').click(function(){
+           
+            // var high = $('#high').val();
+            $.get("/index/audition/ajaxLittle",{data:3},function(data){
+             html = ""
+                // alert(data[1]['course_name']);
+                $.each( data, function(i,v){
+                        // alert(11)
+                                    html += 
+                                                                       '    <div class="tryList">\
+                    <div class="tryPic">\
+                        <a href="{:url('Audition/select')}?id=<?=$value['course_id']?>" uv="7ac4c9e3e9" vid="5fd714ac3217cd34f3774c1ac88ddf37_5">\
+                            <img src="http:_PUBLIC_'+v.course_img+'" title="考场好作文(一)写作的本质" />\
+                        </a>\
+                        <span class="sign grades">'+v.stage_name+'</span>\
+                        <span class="sign object">'+v.subject_name+'</span>\
+                    </div>\
+                    <div class="tryIntro">\
+                        <h6>'+v.course_name+'</h6>\
+                    </div>\
+                </div>\
+                ';
+                });
+
+                // alert(html)
+                $('#senior').html(html);
+                // $('#high').attr('class','active');
+                // $('#first').removeAttr('class');
+                // $('#little').removeAttr('class');
+            })
+        })
+    })
+</script>
+<!--小学试听 -->
+<div class="tryMain clearfix" id="primarySchool" style="display:none;">   
+</div>
+<!--试听 end-->
+<div class="polyplayer-media" style="left: 50%; padding: 9px 10px 30px; display:none;">
+	<div class="vkoplayer-media-box">
+		<span class="close" title="关闭"></span>
+		<div id="poly-play"></div>
+	</div>
+</div>
+<style>
+.polyplayer-media{ width:640px; height:390px; position:fixed; z-index:1100; top:50%; left:-50%; margin:-215px 0 0 -360px; background:url(http://static.vko.cn/v5/js/common/vko/ui/letv/1.0.0/flash/vkomedia.png) no-repeat; padding:9px 10px 30px 10px; _position:absolute;}
+.polyplayer-media .vkoplayer-media-box{ position:relative; z-index:102; background:#101010; overflow:hidden;}
+.polyplayer-media .vkoplayer-media-box h4 span{ float:left; margin:0 0 0 15px;color:#fff; width:auto;}
+.polyplayer-media .close{ width:12px!important; height:12px; background:url(http://static.vko.cn/v5/js/common/vko/ui/player/1.0.0/flash/close.gif) center center no-repeat; float:left; margin:0; position:absolute; top:8px; right:8px; cursor:pointer;}
+</style>
+<script src='http://static.polyv.net/file/polyvplayer_v2.0.min.js'></script>
+<script type="text/javascript">
+	var staticPath = 'http://static.vko.cn'+"/v8"; 
+	var ssoPath = 'http://sso.vko.cn';
+</script>
+<script type="text/javascript" src="http://static.vko.cn/common/js/stacktrace.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/common/js/vkofileloader.js"></script><!--kf-->
+<script type="text/javascript" src="http://static.vko.cn/common/js/seajs/seajs/2.2.0/sea.js"></script>
+<!-- <script type="text/javascript" src="http://static.vko.cn/common/js/config.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/vko/js/config.js"></script> -->
+<script type="text/javascript">try{if(document.URL.indexOf('photo/view')==-1)window.document.domain = "vko.cn";}catch(e){}</script>
+
+<script type="text/javascript" src="http://static.vko.cn/v8/common/js/utils.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/v8/common/js/cookie.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/v8/common/js/vkouser.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/v8/common/js/local/md5.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/v8/common/js/login.js"></script>
+<script type="text/javascript" src="http://static.vko.cn/v8/common/config.js"></script>
+
+<!-- 加载公共部分 -->
+<script src="_PUBLIC_/js/header.js"></script>
+<script src="_PUBLIC_/js/footer.js"></script>
+<!-- 加载公共部分 -->
+
+</body>
+</html>
