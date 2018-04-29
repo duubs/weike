06257af613043829a00017484c2609d8c6635a54
liying2000 <2838103953@qq.com>
2018-04-29 11:24:31 +0800

    试卷

--- admin/application/admin/controller/ExaminationPaper.php
+++ admin/application/admin/controller/ExaminationPaper.php
@@ -18,10 +18,108 @@ class ExaminationPaper extends Controller
      */
     public function examinationPaper()
     {
-        $data = Db::table('micro_paper')->paginate(3);
+        $data = Db::table('micro_paper')->select();
+        //查询试卷试题
+        $question = Db::table('micro_question')->select();
+        //查询试卷科目
+        $subject = Db::table('micro_subject')->select();
+        //查询试卷学段
+        $stage = Db::table('micro_stage_cate')->where('parent_id <= 0')->select();
+        //查询试卷年份
+        $month = Db::table('micro_month')->select();
+        //查询试卷地区
+        $area = Db::table('micro_area')->select();
+
         return view('examination_paper',[
-                                            'data'=>$data
+                                            'data'=>$data,
+                                            'question'=>$question,
+                                            'subject'=>$subject,
+                                            'stage'=>$stage,
+                                            'area'=>$area,
+                                            'month'=>$month
                                         ]);
+    }
+    /**
+     * @Author    Hybrid
+     * @DateTime  2018-04-28
+     * @copyright [copyright]
+     * @license   [license]
+     * @version   [version]
+     * @return    [type]      [查询学段 下拉]
+     */
+    public function examinationDown(){
+       $stage_id = Request::instance()->get('stage_id');
+       $data = Db::table('micro_paper')->alias('paper')
+                ->join('micro_stage_cate stage_cate','paper.stage_cate_id = stage_cate.stage_cate_id')
+                ->where('paper.stage_cate_id','=',$stage_id)->select();
+        return $data;
+        
+    }
+
+    /**
+     * @Author    Hybrid
+     * @DateTime  2018-04-28
+     * @copyright [copyright]
+     * @license   [license]
+     * @version   [version]
+     * @return    [type]      [查询科目 下拉]
+     */
+    public function examinationSubject(){
+        $subject_id = Request::instance()->get('subject_id'); 
+        $data = Db::table('micro_paper')->alias('paper')
+                ->join('micro_subject subject','paper.subject_id = subject.subject_id')
+                ->where('paper.subject_id',$subject_id)->select();
+        return $data;
+        
+    }
+
+    /**
+     * @Author    Hybrid
+     * @DateTime  2018-04-28
+     * @copyright [copyright]
+     * @license   [license]
+     * @version   [version]
+     * @return    [type]      [查询年份 下拉]
+     */
+    public function examinationMonth()
+    {
+        $month_id = Request::instance()->get('month_id'); 
+        $data = Db::table('micro_paper')->alias('paper')
+                ->join('micro_month month','paper.month_id = month.month_id')
+                ->where('paper.month_id',$month_id)->select();
+        return $data;
+    }
+
+    /**
+     * @Author    Hybrid
+     * @DateTime  2018-04-28
+     * @copyright [copyright]
+     * @license   [license]
+     * @version   [version]
+     * @return    [type]      [查询地区 下拉]
+     */
+    public function examinationArea()
+    {
+        $area_id = Request::instance()->get('area_id'); 
+        $data = Db::table('micro_paper')->alias('paper')
+                ->join('micro_area area','paper.area_id = area.area_id')
+                ->where('paper.area_id',$area_id)->select();
+        return $data; 
+    }
+
+    public function examinationGo()
+    {
+       $data = Request::instance()->get();
+       // var_dump($data);die;
+       $res = Db::table('micro_paper')
+              ->where('stage_cate_id',$data['stage_name'])
+              ->where('subject_id',$data['subject'])
+              ->where('month_id',$data['month'])
+              ->where('area_id',$data['area'])
+              ->select();
+
+       return $res;
+
     }
 
     /**
@@ -54,11 +152,23 @@ class ExaminationPaper extends Controller
      */
     public function examinationAdd()
     {
+        //查询试卷试题
         $question = Db::table('micro_question')->select();
+        //查询试卷科目
         $data = Db::table('micro_subject')->select();
+        //查询试卷学段
+        $stage = Db::table('micro_stage_cate')->where('parent_id <= 0')->select();
+        //查询试卷年份
+        $month = Db::table('micro_month')->select();
+        //查询试卷地区
+        $area = Db::table('micro_area')->select();
+
         return view('examination_add',[
                                         'question'=>$question,
-                                        'data'=>$data
+                                        'data'=>$data,
+                                        'stage'=>$stage,
+                                        'month'=>$month,
+                                        'area'=>$area
                                      ]);
 
     }
@@ -74,7 +184,7 @@ class ExaminationPaper extends Controller
     public function examinationDo()
     {
         $data = Request::instance()->post();
-        var_dump($data);die;
+        // var_dump($data);die;
         $data['time'] = time();
         $res = Db::table('micro_paper')->insert($data);
         if ($res)  {
@@ -112,60 +222,19 @@ class ExaminationPaper extends Controller
      */
     public function examinationDamo()
     {
-        // $pagin = 0; // 页数
+        
         $tiaoshu = 2; // 条数
-         $question_id = Request::instance()->get('question_id'); // 获取题型;
-         $pagin = empty(Request::instance()->get('pagin')) ? 0 : Request::instance()->get('pagin'); // 获页数;
-         $data['data'] = Db::table('micro_test')->where('test_question_id',$question_id)->page($pagin.','.$tiaoshu)->select(); //当前页数据
-         $data['zongye'] = ceil(count(Db::table('micro_test')->where('test_question_id',$question_id)->select())/$tiaoshu); // 总页数
-         $data['dangye'] = $pagin; // 当前页数
-
-
-         // $data = Db::table('micro_test')->where('test_question_id',$question_id)->select();
-         // $data['ye'] = $data['data']->render();
-         // dump($data['zongye']);exit;
-         return $data;
-         return json_encode($data);
+        $question_id = Request::instance()->get('question_id'); // 获取题型;
+        $pagin = empty(Request::instance()->get('pagin')) ? 0 : Request::instance()->get('pagin'); // 获页数;
+        $data['data'] = Db::table('micro_test')->where('test_question_id',$question_id)->page($pagin.','.$tiaoshu)->select(); //当前页数据
+        $data['zongye'] = ceil(count(Db::table('micro_test')->where('test_question_id',$question_id)->select())/$tiaoshu); // 总页数
+        $data['dangye'] = $pagin; // 当前页数
+        return $data;
+        return json_encode($data);
 
     }
 
-   /**
-    * @Author    Hybrid
-    * @DateTime  2018-04-20
-    * @copyright [copyright]
-    * @license   [license]
-    * @version   [version]
-    * @return    [type]      [试卷修改页面]
-    */
-    public function examinationUpdata()
-    {
-        $paper_id = Request::instance()->get('paper_id'); // 获取某个get变量;
-        $data = Db::table('micro_paper')->where(['paper_id'=>$paper_id])->select();
-        return view('examination_updata',[
-                                            'data'=>$data[0]
-                                          ]);
 
-    }
-
-    /**
-     * @Author    Hybrid
-     * @DateTime  2018-04-20
-     * @copyright [copyright]
-     * @license   [license]
-     * @version   [version]
-     * @return    [数组]      [修改数据]
-     */
-    public function examinationUpdatado()
-    {
-       
-        $test= Request::instance()->post(); // 获取某个get变量;
-        $paper_id=$test['paper_id'];
-        $res = Db::table('micro_paper')->where(['paper_id'=>$paper_id])->setField($test);
-        if ($res) {
-            return $this->success('修改成功','examinationPaper/examinationPaper','',1);
-        }
-              
-    }
 
 
     /**
@@ -198,6 +267,7 @@ class ExaminationPaper extends Controller
     public function examinationDesc()
     {
         $paper_id= Request::instance()->get('paper_id'); // 获取某个get变量;
+        
 
         $data = Db::table('micro_test')->alias('test')
                 ->join('micro_paper paper','test.test_id=paper.test_ids')->where('paper_id',$paper_id)->select();
--- admin/application/admin/view/examination_paper/examination_paper.php
+++ admin/application/admin/view/examination_paper/examination_paper.php
@@ -35,15 +35,51 @@
                             <div class="am-btn-toolbar">
                                 <div class="am-btn-group am-btn-group-xs">
                                     
-                                   <a href="{:url('ExaminationPaper/examinationAdd')}" class="am-btn am-btn-default am-btn-success" >
-                                        <span class="am-icon-plus"></span> 新增
-                                    </a>       
+                                  <!--  <a href="{:url('ExaminationPaper/examinationAdd')}" class="am-btn am-btn-default am-btn-success" > -->
+                                        <span  class="am-icon-plus" ></span> 新增
+                                    <!-- </a>        -->
                                 </div>
                             </div>
                         </div>
                         <div >   
                         </div>
+                        <div >
+                            <div >
+                                <span style="font-size:18px;">学段</span>
+                                    <select name="stage_name" style="width:60px;height:25px;" id="stage_name">
+                                        <option value="">请选学段</option>
+                                     <?php foreach ($stage as $key => $value): ?>
+                                      <option value="{$value['stage_cate_id']}">{$value['stage_name']}</option>
+                                     <?php endforeach ?>
+                                    </select>
+                                <span style="font-size:18px;">科目</span>
+                                <select name="subject_id" id="subject" style="width:60px;height:25px;">
+                                     <option value="">请选年级</option>
+                                    <?php foreach ($subject as $key => $value): ?>
+                                       <option value="{$value['subject_id']}">{$value['subject_name']}</option> 
+                                    <?php endforeach ?>
+                                   
+                                </select>
+                                 <span style="font-size:18px;">年份</span>
+                                <select name="subject_id" id="month" style="width:60px;height:25px;">
+                                    <option value="">请选年份</option>
+                                    <?php foreach ($month as $key => $value): ?>
+                                        <option value="{$value['month_id']}">{$value['month_name']}</option>
+                                    <?php endforeach ?>
+                                </select>
+                                 <span style="font-size:18px;">地区</span>
+                                <select name="subject_id" id="area" style="width:60px;height:25px;">
+                                    <option value="">请选地区</option>
+                                    <?php foreach ($area as $key => $value): ?>
+                                        <option value="{$value['area_id']}">{$value['area_name']}</option>
+                                    <?php endforeach ?>
+                                </select>
+
+                                <button class="am-btn am-btn-default am-btn-warning" id="go">GO</button>
+                            </div>
+                        </div>
                     </div>
+                     
                     <div class="am-g">
                         <div class="am-u-sm-12">
                             <form class="am-form">
@@ -53,7 +89,6 @@
                                             <th class="table-check"><input type="checkbox" class="tpl-table-fz-check" id="ace"  ></th>
                                             <th class="table-id">编号</th>
                                             <th class="table-title">试卷名称</th>
-                                            <th class="table-author am-hide-sm-only">试卷科目</th>
                                             <th class="table-author am-hide-sm-only">创建时间</th>
                                             <th class="table-author am-hide-sm-only">操作</th>
                                         </tr>
@@ -64,20 +99,17 @@
                                             <td><input type="checkbox" name="box" ></td>
                                             <td><?=$v['paper_id']?></td>
                                             <td><a href="#"><?=$v['paper_name']?></a></td>
-                                            <td class="am-hide-sm-only"><?=$v['subject_name']?></td >
                                             <td class="am-hide-sm-only"><?=date('Y-m-d H:i:s')?></td>
                                             <td>
                                                 <div class="am-btn-toolbar">
                                                     <div class="am-btn-group am-btn-group-xs">
                                                       
-                                                       <a href="{:url('ExaminationPaper/examinationUpdata')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >
-                                                        <span></span> 编辑
-                                                        </a> 
+                                                      
 
                                                             <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">
 
                                                          <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >
-                                                        <span></span> 详情
+                                                        <span></span> 预览试卷
                                                         </a> 
 
                                                     </div>
@@ -90,7 +122,7 @@
                                 <div class="am-cf">
                                     <div class="am-fr">
                                         <ul class="am-pagination tpl-pagination">
-                                          {$data->render()}
+                                          
                                         </ul>
                                     </div>
                                 </div>
@@ -103,9 +135,151 @@
         </div>
     </div>
 </div>
-
     <script src="admin/js/jquery.min.js"></script>
     <script type="text/javascript">
+      $(function(){
+        $('#stage_name').change(function(){
+            var stage_id = $(this).val();
+            $.get('/admin/examinationpaper/examinationDown',{stage_id:stage_id},function(data){
+              
+                var html=""
+                for(k in data){
+                    html+='<tr>\
+                                <td><input type="checkbox" name="box" ></td>\
+                                <td>'+data[k].paper_id+'</td>\
+                                <td><a href="#">'+data[k].paper_name+'</a></td>\
+                                <td class="am-hide-sm-only">'+getLocalTime(data[k].time)+'</td>\
+                                 <td>\
+                                    <div class="am-btn-toolbar">\
+                                        <div class="am-btn-group am-btn-group-xs">\
+                                                <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">\
+                                                <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
+                                                        <span></span> 预览试卷\
+                                                        </a> \
+                                        </div>\
+                                    </div>\
+                                </td>\
+                            </tr>';
+                        }
+                        $('#body').html(html);
+                 })
+
+                 
+
+            })
+
+            $('#subject').change(function(){
+                var subject_id = $(this).val();
+                $.get('/admin/examinationpaper/examinationSubject',{subject_id:subject_id},function(data){
+                    var html =""
+                    for(k in data){
+                        html+='<tr>\
+                                <td><input type="checkbox" name="box" ></td>\
+                                <td>'+data[k].paper_id+'</td>\
+                                <td><a href="#">'+data[k].paper_name+'</a></td>\
+                                <td class="am-hide-sm-only">'+getLocalTime(data[k].time)+'</td>\
+                                 <td>\
+                                    <div class="am-btn-toolbar">\
+                                        <div class="am-btn-group am-btn-group-xs">\
+                                                <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">\
+                                                <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
+                                                        <span></span> 预览试卷\
+                                                        </a> \
+                                        </div>\
+                                    </div>\
+                                </td>\
+                            </tr>';
+                    }
+                    $('#body').html(html);
+                })
+            })
+
+
+            $('#month').change(function(){
+                var month_id = $(this).val();
+                $.get('/admin/examinationpaper/examinationMonth',{month_id:month_id},function(data){
+                    var html =""
+                    for(k in data){
+                        html+='<tr>\
+                                <td><input type="checkbox" name="box" ></td>\
+                                <td>'+data[k].paper_id+'</td>\
+                                <td><a href="#">'+data[k].paper_name+'</a></td>\
+                                <td class="am-hide-sm-only">'+getLocalTime(data[k].time)+'</td>\
+                                 <td>\
+                                    <div class="am-btn-toolbar">\
+                                        <div class="am-btn-group am-btn-group-xs">\
+                                                <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">\
+                                                <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
+                                                        <span></span> 预览试卷\
+                                                        </a> \
+                                        </div>\
+                                    </div>\
+                                </td>\
+                            </tr>';
+                    }
+                    $('#body').html(html);
+                })
+            })
+
+             $('#area').change(function(){
+                var area_id = $(this).val();
+                $.get('/admin/examinationpaper/examinationArea',{area_id:area_id},function(data){
+                    var html =""
+                    for(k in data){
+                        html+='<tr>\
+                                <td><input type="checkbox" name="box" ></td>\
+                                <td>'+data[k].paper_id+'</td>\
+                                <td><a href="#">'+data[k].paper_name+'</a></td>\
+                                <td class="am-hide-sm-only">'+getLocalTime(data[k].time)+'</td>\
+                                 <td>\
+                                    <div class="am-btn-toolbar">\
+                                        <div class="am-btn-group am-btn-group-xs">\
+                                                <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">\
+                                                <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
+                                                        <span></span> 预览试卷\
+                                                        </a> \
+                                        </div>\
+                                    </div>\
+                                </td>\
+                            </tr>';
+                    }
+                    $('#body').html(html);
+                })
+            })
+
+             $('#go').click(function(){
+               var stage_name = $('#stage_name').val();
+               var subject = $('#subject').val();
+               var month = $('#month').val();
+               var area = $('#area').val();
+              $.get("/admin/examinationpaper/examinationGo",{stage_name:stage_name,subject:subject,month:month,area:area},function(data) {
+                var html=''
+                for(k in data){
+                    html+='<tr>\
+                                <td><input type="checkbox" name="box" ></td>\
+                                <td>'+data[k].paper_id+'</td>\
+                                <td><a href="#">'+data[k].paper_name+'</a></td>\
+                                <td class="am-hide-sm-only">'+getLocalTime(data[k].time)+'</td>\
+                                 <td>\
+                                    <div class="am-btn-toolbar">\
+                                        <div class="am-btn-group am-btn-group-xs">\
+                                                <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">\
+                                                <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
+                                                        <span></span> 预览试卷\
+                                                        </a> \
+                                        </div>\
+                                    </div>\
+                                </td>\
+                            </tr>';
+                }
+                 $('#body').html(html);
+              })
+                 
+
+             })
+            
+        })
+      
       $(function(){
         $('#delete').click(function(){
           var paper_id = $(this).attr('name');
@@ -143,12 +317,9 @@
                                  <td>\
                                     <div class="am-btn-toolbar">\
                                         <div class="am-btn-group am-btn-group-xs">\
-                                           <a href="{:url('ExaminationPaper/examinationUpdata')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
-                                            <span></span> 编辑\
-                                            </a> \
                                                 <input type="button" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" id="delete" name="<?=$v['paper_id']?>" value="删除">\
                                                 <a href="{:url('ExaminationPaper/examinationDesc')}?paper_id={$v['paper_id']}" class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" >\
-                                                        <span></span> 详情\
+                                                        <span></span> 预览试卷\
                                                         </a> \
                                         </div>\
                                     </div>\
